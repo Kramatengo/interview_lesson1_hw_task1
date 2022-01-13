eaa5b0de3944b250f9eea7781c75f559d26e3211
Alexander Kramarenko <88139085+Kramatengo@users.noreply.github.com>
2022-01-13 20:08:42 +0300

    Add files via upload

---
+++ Client.java
@@ -0,0 +1,5 @@
+package ru.alexander_kramarenko.builder_1;
+
+public class Client {
+
+}
---
+++ ComposeClient.java
@@ -0,0 +1,10 @@
+package ru.alexander_kramarenko.builder_1;
+
+public class ComposeClient {
+
+    public static void main(String[] args) {
+
+    }
+
+
+}
